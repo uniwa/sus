106b3ca8cf2e7f301c8951ba4e2a468cd07e066e
Dimosthenis Nikoudis <dnna@dnna.gr>
2014-05-27 19:31:54 +0300

    SSO enhancements

--- src/SUS/AdminBundle/Controller/AuthController.php
+++ src/SUS/AdminBundle/Controller/AuthController.php
@@ -2,10 +2,27 @@
 
 namespace SUS\AdminBundle\Controller;
 
+use BeSimple\SsoAuthBundle\Sso\Manager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AuthController extends Controller {
+    public function loginAction(Manager $manager, Request $request, AuthenticationException $exception = null) {
+        /*if(isset($exception)) {
+            var_dump($exception);
+            die();
+        }*/
+        return new RedirectResponse($manager->getServer()->getLoginUrl());
+    }
+
+    public function logoutAction(Manager $manager, Request $request) {
+        return new RedirectResponse(strtok($manager->getServer()->getLogoutUrl(), '?'));
+    }
+
     public function debugAction() {
         $request = $this->getRequest();
         $session = $request->getSession();
