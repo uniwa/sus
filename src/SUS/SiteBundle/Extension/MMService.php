0a28877b1b087d6a05f4ef57a203134245917bc6
Dimosthenis Nikoudis <dnna@dnna.gr>
2014-06-15 23:04:42 +0300

    Make mmsch username and pass a parameter

--- src/SUS/SiteBundle/Extension/MMService.php
+++ src/SUS/SiteBundle/Extension/MMService.php
@@ -128,8 +128,8 @@ class MMService {
     }
 
     protected function queryMM($resource, $params = array()) {
-        $username = "mmsch";
-        $password = "mmsch";
+        $username = $this->container->getParameter('mmsch_username');
+        $password = $this->container->getParameter('mmsch_password');
         $server = 'http://mmsch.teiath.gr/api/'.$resource;
 
         $curl = curl_init ($server);
@@ -208,8 +208,8 @@ class MMService {
 
         $curl = curl_init("http://mmsch.teiath.gr/api/units");
 
-        $username = 'mmschadmin';
-        $password = 'mmschadmin';
+        $username = $this->container->getParameter('mmsch_username');
+        $password = $this->container->getParameter('mmsch_password');
         curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($curl, CURLOPT_USERPWD,  $username.":".$password);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
