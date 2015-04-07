7a40b2cf4b8455802df3e1af246bb755e58f1d34
Dimosthenis Nikoudis <dnna@dnna.gr>
2015-04-07 15:58:10 +0300

    Changed preUpdate to onFlush to prevent only a single entity being updated

--- src/SUS/SiteBundle/Extension/MMSyncableListener.php
+++ src/SUS/SiteBundle/Extension/MMSyncableListener.php
@@ -2,7 +2,8 @@
 
 namespace SUS\SiteBundle\Extension;
 
-use Doctrine\Common\EventArgs;
+//use Doctrine\Common\EventArgs;
+use Doctrine\ORM\Event\OnFlushEventArgs;
 use Symfony\Component\HttpKernel\KernelInterface;
 use SUS\SiteBundle\Entity\MMSyncableEntity;
 
@@ -16,7 +17,7 @@ class MMSyncableListener
         $this->kernel = $kernel;
     }
 
-    public function prePersist(EventArgs $args) {
+    /*public function prePersist(EventArgs $args) {
         $entity = $args->getEntity();
         if(!$entity instanceof MMSyncableEntity) {
             return;
@@ -39,6 +40,30 @@ class MMSyncableListener
             $uow = $em->getUnitOfWork();
             $meta = $em->getClassMetadata(get_class($entity));
             $uow->recomputeSingleEntityChangeSet($meta, $entity);
+        }
+    }*/
+
+    public function onFlush(OnFlushEventArgs $args)
+    {
+        $em = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        $entities = array_merge(
+            $uow->getScheduledEntityInsertions(),
+            $uow->getScheduledEntityUpdates()
+        );
+
+        foreach ($entities as $entity) {
+            if(!$entity instanceof MMSyncableEntity) {
+                continue;
+            }
+
+            if($this->kernel->getEnvironment() == 'prod') {
+                $this->mmservice->persistMM($entity);
+                $em->persist($entity);
+                $meta = $em->getClassMetadata(get_class($entity));
+                $uow->recomputeSingleEntityChangeSet($meta, $entity);
+            }
         }
     }
 
